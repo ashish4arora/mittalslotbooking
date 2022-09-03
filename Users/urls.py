7d2cf26af431c6ffdcda1044b7c69a055e9a97e6
ashish <ashisharora.dev@gmail.com>
2022-09-04 01:36:14 +0530

    .

--- Users/urls.py
+++ Users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', views.landing, name = "landing"),
@@ -10,6 +12,6 @@ urlpatterns = [
     path('denied', views.accessdenied , name = "accessdenied"),
     path('assignstaff', views.assignstaff, name = "assignstaff" ),
     path('revokestaff/<str:pk>', views.revokestaff, name = "revokestaff"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
